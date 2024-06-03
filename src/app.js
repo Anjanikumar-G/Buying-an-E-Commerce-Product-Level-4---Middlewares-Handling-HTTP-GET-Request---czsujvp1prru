82c1f8b29fdf6c9f785d4286a521233b006ecccf
Anjanikumar Guruvali <108091230+Anjanikumar-G@users.noreply.github.com>
2024-06-03 13:06:12 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -12,58 +12,32 @@ const products = JSON.parse(
 // Middlewares
 app.use(express.json());
 
-// PATCH endpoint for updating product data
-app.patch('/api/v1/products/:id',(req,res)=>{
-    const id = req.params.id * 1;
-    const product = products.find(product => product.id===id);
-    if (!product){
-        return res.status(404).send({
-            status: "failed",
+// Write PATCH endpoint to buy a product for the client here
+// Endpoint /api/v1/products/:id
+
+app.patch('/api/v1/products/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    const product = products.find((entry) => id === entry.id);
+    if(!product){
+        return res.status(404).json({
+            status: 'failed',
             message: "Product not found!"
         })
     }
-
-    product.quantity -= 1;
-
-    if(product.quantity>=0){
-        return res.status(200).json({
-                status : "success",
-                message :`Thank you for purchasing ${product.name}`,
-                product 
-        });
+    if(product.quantity < 1){
+        return res.status(404).json({
+            "status": "success", "message": `${product.name} , Out of stock!`
+        })
     }
+    product.quantity -= 1;
+    return res.status(200).json({
+        "status": "success",
+        "message": `Thank you for purchasing ${product.name}`,
+        product
+    })
+})
 
-    
-    return res.status(404).json({
-        status : "success",
-        message :`${product.name}, Out of stock!`,
-    });
-
-    
-});
 
 
 
 module.exports = app;
-
-// const fs = require('fs');
-// const express = require('express');
-// const app = express();
-
-
-// // Importing products from products.json file
-// const products = JSON.parse(
-//     fs.readFileSync(`${__dirname}/data/product.json`)
-// );
-
-
-// // Middlewares
-// app.use(express.json());
-
-// // Write PATCH endpoint to buy a product for the client here
-// // Endpoint /api/v1/products/:id
-
-
-
-
-// module.exports = app;
